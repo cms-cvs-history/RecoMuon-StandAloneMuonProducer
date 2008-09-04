8d7cf02b5d2778ed1a905321f1712a6297f98ab6
cmsbuild <>
2008-09-04 01:30:32 +0000

    This commit was manufactured by cvs2git to create tag 'RecoMuon- StandAloneMuonProducer-CMSSW_2_2_6_HLT'.
    Sprout from master 2008-03-28 10:16:22 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'adding new py configs'
    Cherrypick from master 2008-06-19 14:59:04 UTC Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch> 'cosmic reco in 2.1.0':
        data/standAloneCosmicMuons.cff
    Cherrypick from master 2008-08-01 12:03:04 UTC Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch> 'remove spurious import of Geomtry.CMSCommonData.cmsIdealGeometryXML.cff that should not be done insided reco configurations and only in the top level cfg':
        python/standAloneMuons_cff.py
    Cherrypick from RecoMuon-StandAloneMuonProducer-Br210 2008-09-04 01:30:03 UTC cliu <sha1-14adb22ac61aa8a6f2961b078ceb270851433f16@cern.ch> 'fix CSC segments':
        data/standAloneMuons.cff
        data/standAloneMuons.cfi
        python/standAloneCosmicMuons_cff.py
        python/standAloneMuons_cfi.py
    Delete:
        python/__init__.py

--- python/standAloneMuons_cfi.py
+++ python/standAloneMuons_cfi.py
@@ -8,38 +8,8 @@ standAloneMuons = cms.EDProducer("StandAloneMuonProducer",
     MuonServiceProxy,
     InputObjects = cms.InputTag("MuonSeed"),
     STATrajBuilderParameters = cms.PSet(
-        # a precise propagation direction can be choosen accordingly with the 
-        # above seed position
-        SeedPropagator = cms.string('SteppingHelixPropagatorAny'),
-        NavigationType = cms.string('Standard'),
-        SmootherParameters = cms.PSet(
-            NumberOfSigma = cms.double(3.0),
-            MaxChi2 = cms.double(25.0),
-            Propagator = cms.string('SteppingHelixPropagatorAlong'),
-            ErrorRescalingFactor = cms.double(10.0)
-        ),
-        # where you want the seed (in,out)
-        SeedPosition = cms.string('in'),
-        BWFilterParameters = cms.PSet(
-            NumberOfSigma = cms.double(3.0),
-            BWSeedType = cms.string('fromGenerator'),
-            FitDirection = cms.string('outsideIn'),
-            DTRecSegmentLabel = cms.InputTag("dt4DSegments"),
-            MaxChi2 = cms.double(25.0),
-            MuonTrajectoryUpdatorParameters = cms.PSet(
-                MaxChi2 = cms.double(25.0),
-                RescaleError = cms.bool(False),
-                RescaleErrorFactor = cms.double(100.0),
-                Granularity = cms.int32(2)
-            ),
-            EnableRPCMeasurement = cms.bool(True),
-            CSCRecSegmentLabel = cms.InputTag("cscSegments"),
-            EnableDTMeasurement = cms.bool(True),
-            RPCRecSegmentLabel = cms.InputTag("rpcRecHits"),
-            Propagator = cms.string('SteppingHelixPropagatorAny'),
-            EnableCSCMeasurement = cms.bool(True)
-        ),
-        RefitterParameters = cms.PSet(
+        DoRefit = cms.bool(False),
+        FilterParameters = cms.PSet(
             NumberOfSigma = cms.double(3.0),
             FitDirection = cms.string('insideOut'),
             DTRecSegmentLabel = cms.InputTag("dt4DSegments"),
@@ -57,9 +27,38 @@ standAloneMuons = cms.EDProducer("StandAloneMuonProducer",
             Propagator = cms.string('SteppingHelixPropagatorAny'),
             EnableCSCMeasurement = cms.bool(True)
         ),
-        DoSmoothing = cms.bool(False),
-        DoBackwardRefit = cms.bool(True)
+        # a precise propagation direction can be choosen accordingly with the 
+        # above seed position
+        SeedPropagator = cms.string('SteppingHelixPropagatorAny'),
+        NavigationType = cms.string('Standard'),
+        DoBackwardFilter = cms.bool(True),
+        # where you want the seed (in,out)
+        SeedPosition = cms.string('in'),
+        BWFilterParameters = cms.PSet(
+            NumberOfSigma = cms.double(3.0),
+            BWSeedType = cms.string('fromGenerator'),
+            FitDirection = cms.string('outsideIn'),
+            DTRecSegmentLabel = cms.InputTag("dt4DSegments"),
+            MaxChi2 = cms.double(100.0),
+            MuonTrajectoryUpdatorParameters = cms.PSet(
+                MaxChi2 = cms.double(100.0),
+                RescaleError = cms.bool(False),
+                RescaleErrorFactor = cms.double(100.0),
+                Granularity = cms.int32(2)
+            ),
+            EnableRPCMeasurement = cms.bool(True),
+            CSCRecSegmentLabel = cms.InputTag("cscSegments"),
+            EnableDTMeasurement = cms.bool(True),
+            RPCRecSegmentLabel = cms.InputTag("rpcRecHits"),
+            Propagator = cms.string('SteppingHelixPropagatorAny'),
+            EnableCSCMeasurement = cms.bool(True)
+        ),
+        RefitterParameters = cms.PSet(
+            FitterName = cms.string('KFFitterSmootherSTA'),
+            Option = cms.int32(1)
+        )
     )
 )
+
 
 
